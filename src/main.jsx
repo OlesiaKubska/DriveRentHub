7c77de88c15ca519ada1932c23f45b0662e57f11
OlesiaKubska <kublesia0908@gmail.com>
2024-01-21 07:52:40 +0100

    Normalize line endings

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
  <React.StrictMode>
   <ThemeProvider theme={theme}>
    <GlobalStyles />
-   <BrowserRouter>
+   <BrowserRouter basename="/">
     <Provider store={store}>
      <PersistGate loading={null} persistor={persistor}>
       <App />
